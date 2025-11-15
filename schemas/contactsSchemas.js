75c64fa1b7303a350b2ec72ddbc14664a553d49c
Pavlo <pavlo.rohozhyn@gmail.com>
2025-11-15 22:29:50 +0100

    swagger and  get, get by id, post,  delete, put -not finished

--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -1,9 +1,29 @@
 import Joi from "joi";
 
+/**
+ *
+ */
 export const createContactSchema = Joi.object({
+  name: Joi.string().min(3).max(255).required().messages({
+    "string.empty": `"name" cannot be an empty`,
+    "string.min": `"name" should have a minimum length of {#limit}`,
+    "string.max": `"name" should have a minimum length of {#limit}`,
+    "any.required": `"name" is a required`,
+  }),
 
-})
+  email: Joi.string().email().required().messages({
+    "string.email": `Please enter a valid email address`,
+    "string.empty": `"email" address cannot be empty.`,
+    "any.required": `"email" is a required`,
+  }),
 
-export const updateContactSchema = Joi.object({
+  phone: Joi.string().required().messages({
+    "string.empty": `"phone" address cannot be empty.`,
+    "any.required": `"phone" is a required`,
+  }),
+});
 
-})
+/**
+ *
+ */
+export const updateContactSchema = Joi.object({});
